50823dbe59be601b1561c34440df5ddaf5702b28
Essam.Adel <Essam.Adel@HQOSGIT05>
2023-09-23 15:44:55 +0300

    Add project files.

---
+++ DesktopContactsApp/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System.Windows;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact selectedContact;
+        public ContactDetailsWindow(Contact selectedContact)
+        {
+            InitializeComponent();
+
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            this.selectedContact = selectedContact;
+            nameTextBox.Text = this.selectedContact.Name;
+            emailTextBox.Text = this.selectedContact.Email;
+            phoneTextBox.Text = this.selectedContact.Phone;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            selectedContact.Name = nameTextBox.Text;
+            selectedContact.Phone = phoneTextBox.Text;
+            selectedContact.Email = emailTextBox.Text;
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                conn.Update(selectedContact);
+            }
+
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                conn.Delete(selectedContact);
+            }
+
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            selectedContact = new Contact();
+            Close();
+        }
+    }
+}
---
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopContactsApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+
+
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(
+                new Contact() {Name = "Default or empty name", Phone = "Default or empty phone number"
+                , Email = "Default or empty email"},setText));
+
+        private static void setText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactControl control = d as ContactControl;
+            if(control != null)
+            {
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+
+            }
+        }
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
